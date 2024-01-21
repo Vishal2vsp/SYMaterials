14bc541144158a6b2947ff2d49f6ae9cb0eace6e
Vishal2vsp <vishalpise734@gmail.com>
2024-01-21 23:20:49 +0530

    copyright github

--- theme.config.tsx
+++ theme.config.tsx
@@ -4,14 +4,14 @@ import { DocsThemeConfig } from 'nextra-theme-docs'
 const config: DocsThemeConfig = {
   logo: <span>My Project</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/Vishal2vsp/SYMATERIALS',
   },
   chat: {
     link: 'https://discord.com',
   },
   docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'Copyright © Vishal Pise',
   },
 }
 
